7c131a55b3ea1761eb7910a1ab102d2f97bb8ab5
gwadadasol <gadadasol@gmail.com>
2018-06-09 14:12:12 +0800

    Just add a space to test integration in PyCharm

--- src/ann.py
+++ src/ann.py
@@ -27,6 +27,7 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values # Remove useless cloumn, here the first 3 columns
 y = dataset.iloc[:, 13].values
 
+
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
